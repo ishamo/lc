0e8f022adec9baef1ff3dad668085e68f9b0429c
ishamo <qgmfky@gmail.com>
2019-04-19 15:21:48 +0800

    Accepted

--- 100.same-tree.py
+++ 100.same-tree.py
@@ -4,7 +4,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+#
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -12,4 +12,23 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        
+        if not q and not p: return True
+        if p and q:
+            if p.val == q.val:
+                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return False
+#
+#
+# if __name__ == '__main__':
+#     n1 = TreeNode(1)
+#     n2 = TreeNode(2)
+#     n3 = TreeNode(3)
+#     n4 = TreeNode(4)
+#     n5 = TreeNode(5)
+#     n1.left, n1.right = n2, n3
+#     n2.left = n4
+#     n3.right = n5
+#
+#     s = Solution()
+#     print(s.isSameTree(n1, n1))
+#     print(s.isSameTree(n1, n2))
