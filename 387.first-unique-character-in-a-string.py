15a1b558c17670664cfed961ba30143eebb1b5ba
ishamo <qgmfky@gmail.com>
2019-04-24 14:24:59 +0800

    Accepted

--- 387.first-unique-character-in-a-string.py
+++ 387.first-unique-character-in-a-string.py
@@ -4,4 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+        arr = [0] * 26
+        for i in s:
+            arr[ord(i)-ord('a')] += 1
+        for idx, i in enumerate(s):
+            if arr[ord(i)-ord('a')] == 1:
+                return idx
+        return -1
