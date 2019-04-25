9d12d0d728426aca27107466ad1062d7d0b23a03
ishamo <qgmfky@gmail.com>
2019-04-25 21:44:48 +0800

    Accepted

--- 6.zigzag-conversion.py
+++ 6.zigzag-conversion.py
@@ -5,4 +5,32 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
-        
+        if numRows == 1: return s
+        result = [['' for i in range(len(s))] for j in range(numRows)]
+        i, j = 0, 0
+        direction = 'down'
+        for char in s:
+            result[i][j] = char
+            if direction == 'down':
+                if i == numRows - 1:
+                    direction = 'up'
+                    i -= 1
+                    j += 1
+                else:
+                    i += 1
+            else:
+                if i == 0:
+                    direction = 'down'
+                    i += 1
+                else:
+                    i -= 1
+                    j += 1
+        ret = ''
+        for r in result:
+            ret += ''.join(r)
+        return ret
+#
+#
+# if __name__ == '__main__':
+#     s = Solution()
+#     print(s.convert("ABC", 2))
