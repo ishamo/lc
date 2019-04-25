487598d76b21f54fdb40d15d92b5597abb1a607f
ishamo <qgmfky@gmail.com>
2019-04-25 21:05:19 +0800

    Accepted 两边往中间夹, 值小的往中间缩

--- 11.container-with-most-water.py
+++ 11.container-with-most-water.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        
+        length = len(height)
+        l, r = 0, length - 1
+        water = 0
+        while l < r:
+            area = min(height[l], height[r]) * (r - l)
+            if area > water: water = area
+            mn = l if height[l] < height[r] else r
+            if mn == l:
+                l += 1
+            else:
+                r -= 1
+        return water
