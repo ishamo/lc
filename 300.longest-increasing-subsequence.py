c66c23f4a920f23db139d197170c6793764c3c5a
ishamo <qgmfky@gmail.com>
2019-05-08 21:18:23 +0800

    AC: 300

--- 300.longest-increasing-subsequence.py
+++ 300.longest-increasing-subsequence.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums: return 0
+        DP = [1] * len(nums)
+
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j] and DP[i] < DP[j] + 1:
+                    DP[i] = DP[j] + 1
+        return max(DP)
+
+
+# [-2, -1]
