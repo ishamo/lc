2476b65c4a43dd2f05c2497874e3db003461044b
ishamo <qgmfky@gmail.com>
2019-05-13 20:36:31 +0800

    AC: 402

--- 402.remove-k-digits.py
+++ 402.remove-k-digits.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Solution(object):
     def removeKdigits(self, num, k):
         """
@@ -5,4 +6,21 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        
+        lenc = len(num)
+        if not num or lenc <= k: return '0'
+
+        stack = []
+        for i in range(lenc):
+            if not stack:
+                stack.append(num[i])
+            else:
+                # print('stack, len, need, last, current', stack, len(stack)+lenc-(i+1), lenc-k, stack[-1], num[i])
+                while stack and len(stack) + lenc - (i+1) >= lenc - k and int(stack[-1]) > int(num[i]):
+                    stack.pop(-1)
+                stack.append(num[i])
+        return ''.join(stack[0:lenc-k]).lstrip('0') or '0'
+
+
+# s = Solution()
+# print s.removeKdigits('1432219', 3)
+# print s.removeKdigits('112', 1)
