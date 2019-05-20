a0906bf0c50e9b75617216ac271904c98c89c33a
ishamo <qgmfky@gmail.com>
2019-05-20 14:25:43 +0800

    AC: 342

--- 342.power-of-four.py
+++ 342.power-of-four.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        
+        if num <= 0: return False
+        if num & num - 1: return False
+        while num > 1:
+            num = num >> 2
+        if not num == 1: return False
+        return True
+#
+#
+# s = Solution()
+# print s.isPowerOfFour(3)
+# print s.isPowerOfFour(8)
