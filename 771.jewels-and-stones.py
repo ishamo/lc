7286446ebc570beda525e914e49027f5e1f86c47
ishamo <qgmfky@gmail.com>
2019-05-22 18:46:58 +0800

    AC: 771

--- 771.jewels-and-stones.py
+++ 771.jewels-and-stones.py
@@ -5,4 +5,12 @@ class Solution(object):
         :type S: str
         :rtype: int
         """
-        
+        table = {}
+        for x in J: table[x] = 1
+        count = 0
+        for s in S:
+            if s in table:
+                count += 1
+
+        return count
+
