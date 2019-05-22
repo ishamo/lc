85186cb6cd2b3ccaf00830b0b902fa05f51eacaa
ishamo <qgmfky@gmail.com>
2019-05-22 20:30:42 +0800

    AC: 278

--- 278.first-bad-version.py
+++ 278.first-bad-version.py
@@ -9,4 +9,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        l, r = 1, n
+        while l < r:
+            mid = (l + r) / 2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
