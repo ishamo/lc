e743783aefcbe494b3d899b974b0b43daa544eba
ishamo <qgmfky@gmail.com>
2019-05-23 22:04:41 +0800

    AC: 513

--- 513.find-bottom-left-tree-value.py
+++ 513.find-bottom-left-tree-value.py
@@ -11,4 +11,44 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        
+        queue = [(root, 1)]
+        while queue:
+            lenq = len(queue)
+            for node in queue[:lenq]:
+                if node[0].left:
+                    queue.append((node[0].left, 1))
+                if node[0].right:
+                    queue.append((node[0].right, 0))
+
+            if len(queue) == lenq:
+                for item in queue:
+                    if item[1]:
+                        return item[0].val
+
+                return queue[0][0].val
+
+            else:
+                queue = queue[lenq:]
+
+        return -1
+#
+#
+# n1 = TreeNode(1)
+# n2 = TreeNode(2)
+# n3 = TreeNode(3)
+# n4 = TreeNode(4)
+# n5 = TreeNode(5)
+# n6 = TreeNode(6)
+# n7 = TreeNode(7)
+#
+# n1.left, n1.right = n2, n3
+# n2.left = n4
+# n3.left, n3.right = n5, n6
+# n5.left = n7
+#
+# n1 = TreeNode(1)
+# n2 = TreeNode(1)
+# n1.right = n2
+#
+# s = Solution()
+# print s.findBottomLeftValue(n1)
