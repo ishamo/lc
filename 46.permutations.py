dfec89abde911eb909c03879c97bc2ef9c5a6967
ishamo <qgmfky@gmail.com>
2019-05-23 22:46:35 +0800

    AC: 46

--- 46.permutations.py
+++ 46.permutations.py
@@ -4,4 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        if not nums: return []
+        result = []
+        lenn = len(nums)
+        pool = list(range(lenn))
+
+        def enum_value(current, pool):
+            if len(pool) == 1:
+                current.append(nums[pool[0]])
+                result.append(current)
+                return
+            else:
+                for p in pool:
+                    tmp = pool[:]
+                    tmp.remove(p)
+                    enum_value(current + [nums[p]], tmp)
+
+        enum_value([], pool)
+        return result
