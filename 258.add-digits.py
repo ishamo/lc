14f0eeb74bcf9ed7a88ae31743fccac72798d4fc
ishamo <qgmfky@gmail.com>
2019-05-24 11:34:21 +0800

    AC: 258

--- 258.add-digits.py
+++ 258.add-digits.py
@@ -4,4 +4,10 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        
+        numstr = str(num)
+        while len(numstr) > 1:
+            nums = [int(item) for item in list(numstr)]
+            numstr = str(sum(nums))
+
+        return int(numstr)
+
