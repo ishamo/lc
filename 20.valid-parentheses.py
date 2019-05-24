bc9f6587e57765064cb467de76d6d719593e1521
ishamo <qgmfky@gmail.com>
2019-05-24 10:57:18 +0800

    AC: 20

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        
+        stack = []
+        table = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        if not s: return True
+        for c in s:
+            if c in ['(', '{', '[']:
+                stack.append(c)
+            else:
+                if not stack or not stack[-1] == table[c]: return False
+                stack.pop(-1)
+
+        if stack: return False
+        return True
+
