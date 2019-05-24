c17ea7840168c2cee0d3515cf9614c8d0adea7d0
ishamo <qgmfky@gmail.com>
2019-05-24 14:57:15 +0800

    AC: 222

--- 222.count-complete-tree-nodes.py
+++ 222.count-complete-tree-nodes.py
@@ -11,4 +11,15 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        
+        if not root: return 0
+        cnt = [0]
+        self.do_the_cnt(root, cnt)
+
+        return cnt[0]
+
+    def do_the_cnt(self, root, cnt):
+        cnt[0] += 1
+        if root.left:
+            self.do_the_cnt(root.left, cnt)
+        if root.right:
+            self.do_the_cnt(root.right, cnt)
