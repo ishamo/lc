8179f9dff50c799aa92909a0002f3d3b803dbe35
ishamo <qgmfky@gmail.com>
2019-05-24 10:49:42 +0800

    AC: 290

--- 290.word-pattern.py
+++ 290.word-pattern.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type str: str
         :rtype: bool
         """
-        
+        table = {}
+        slist = str.split()
+        if not len(slist) == len(pattern): return False
+        for idx, p in enumerate(pattern):
+            if p not in table:
+                table[p] = slist[idx]
+            else:
+                if table[p] != slist[idx]: return False
+
+        values = table.values()
+        if len(set(values)) < len(values): return False
+
+        return True
