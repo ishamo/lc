590dd052797f6799bf27367b8ab98e84173b5ae0
ishamo <qgmfky@gmail.com>
2019-05-24 13:11:22 +0800

    AC: 242

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -5,4 +5,20 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        
+        if not s and not t: return True
+        lens, lent = len(s), len(t)
+        if not lens == lent: return False
+
+        alist = [0] * 26
+        blist = [0] * 26
+
+        for i in s:
+            alist[ord(i)-ord('a')] += 1
+
+        for j in t:
+            blist[ord(j)-ord('a')] += 1
+
+        for k in range(len(alist)):
+            if not alist[k] == blist[k]: return False
+
+        return True
