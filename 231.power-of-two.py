cadef930fc61a74dbe48f2c25fd1cf67671be66a
ishamo <qgmfky@gmail.com>
2019-05-24 13:15:44 +0800

    AC: 231

--- 231.power-of-two.py
+++ 231.power-of-two.py
@@ -4,4 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        
+        if n & n-1: return False
+
+        while n > 1:
+            n = n >> 1
+
+        return True if n == 1 else False
