4198bb068422a705cd673b7842f4f179c26d284b
ishamo <qgmfky@gmail.com>
2019-05-27 15:08:03 +0800

    AC: 441

--- 441.arranging-coins.py
+++ 441.arranging-coins.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        total = 0
+        i = 1
+        level = 0
+        while total+i <= n:
+            total += i
+            i += 1
+            level += 1
+
+        return level
