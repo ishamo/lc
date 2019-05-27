5f6d9d67c0d20e27b44d72277b4db6b89cb96af8
ishamo <qgmfky@gmail.com>
2019-05-27 14:59:52 +0800

    AC: 205

--- 205.isomorphic-strings.py
+++ 205.isomorphic-strings.py
@@ -5,4 +5,13 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        
+        if not len(s) == len(t): return False
+        table = {}
+        for idx, c in enumerate(s):
+            if c not in table:
+                table[c] = t[idx]
+            else:
+                if not table[c] == t[idx]: return False
+
+        values = table.values()
+        return True if len(set(values)) == len(values) else False
