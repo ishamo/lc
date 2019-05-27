b435338a092b95ad06280e863a8b7d28c4b03b00
ishamo <qgmfky@gmail.com>
2019-05-27 19:14:05 +0800

    AC: 350

--- 350.intersection-of-two-arrays-ii.py
+++ 350.intersection-of-two-arrays-ii.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        
+        result = []
+        if not nums1 or not nums2: return result
+        len1 = len(nums1)
+        pool = [0] * len1
+
+        for i in nums2:
+            for idx, j in enumerate(nums1):
+                if i == j and not pool[idx]:
+                    result.append(i)
+                    pool[idx] = 1
+                    break
+
+        return result
