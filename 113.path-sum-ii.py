37225064944120675e6e44475085f8daae2b2201
ishamo <qgmfky@gmail.com>
2019-05-28 14:17:07 +0800

    AC: 113

--- 113.path-sum-ii.py
+++ 113.path-sum-ii.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def pathSum(self, root, sum):
@@ -12,4 +12,48 @@ class Solution(object):
         :type sum: int
         :rtype: List[List[int]]
         """
-        
+        path = []
+        def travel_for_path(root, pth):
+            if not root: return
+            p = pth[:]
+            p.append(root.val)
+            if not root.left and not root.right:
+                path.append(p)
+            else:
+                if root.left:
+                    travel_for_path(root.left, p)
+                if root.right:
+                    travel_for_path(root.right, p)
+
+        travel_for_path(root, [])
+        result = []
+        # print('path', path)
+        for p in path:
+            val = reduce(lambda x, y: x+y, p)
+            if val == sum:
+                result.append(p)
+
+        return result
+#
+#
+# n1 = TreeNode(5)
+# n2 = TreeNode(4)
+# n3 = TreeNode(8)
+# n4 = TreeNode(11)
+# n5 = TreeNode(13)
+# n6 = TreeNode(4)
+# n7 = TreeNode(7)
+# n8 = TreeNode(2)
+# n9 = TreeNode(5)
+# n10 = TreeNode(1)
+#
+# n1.left, n1.right = n2, n3
+# n2.left = n4
+# n3.left, n3.right = n5, n6
+# n4.left, n4.right = n7, n8
+# n6.left, n6.right = n9, n10
+#
+#
+# s = Solution()
+# print s.pathSum(n1, 22)
+#
