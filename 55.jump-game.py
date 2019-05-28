4be5fe037228e96bbaee22bf98f766a438bd5fe2
ishamo <qgmfky@gmail.com>
2019-05-28 13:11:58 +0800

    AC: 55

--- 55.jump-game.py
+++ 55.jump-game.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        
+        if not nums: return False
+        if  len(nums) == 1: return True
+        lenn = len(nums)
+        DP = [0] * len(nums)
+        DP[0] = nums[0]
+        for i in range(1, lenn):
+            if DP[i-1] < 1: DP[i] = 0
+            else:
+                DP[i] = max(DP[i-1]-1, nums[i])
+
+        # print 'DP', DP
+        return DP[-2] >= 1
