59d78367e5a11443afc0654749a36d30cc046217
ishamo <qgmfky@gmail.com>
2019-05-30 23:10:46 +0800

    AC: 674

--- 674.longest-continuous-increasing-subsequence.py
+++ 674.longest-continuous-increasing-subsequence.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums: return 0
+        lenn = len(nums)
+        DP = [1] * lenn
+
+        for i in range(1, lenn):
+            if nums[i] > nums[i-1]:
+                DP[i] = DP[i-1] + 1
+
+        return max(DP)
