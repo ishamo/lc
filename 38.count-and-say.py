ebb2f8b567b2bd6dd3eb4f883e7cd5afca562509
ishamo <qgmfky@gmail.com>
2019-05-30 11:46:21 +0800

    AC: 38

--- 38.count-and-say.py
+++ 38.count-and-say.py
@@ -4,4 +4,26 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-        
+        current = '1'
+        for i in range(1, n):
+            cnt = 0
+            newstr = ''
+            for idx, item in enumerate(current):
+                if idx == 0:
+                    cnt = 1
+                    continue
+
+                if current[idx] == current[idx-1]:
+                    cnt += 1
+                else:
+                    newstr += str(cnt) + current[idx-1]
+                    cnt = 1
+
+            newstr += str(cnt) + current[-1]
+            current = newstr
+        return current
+#
+#
+# s = Solution()
+# for i in range(1, 6):
+#     print s.countAndSay(i)
