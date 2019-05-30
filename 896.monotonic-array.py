01798f252553b2438426fb8e5a5a7a9ef21fe211
ishamo <qgmfky@gmail.com>
2019-05-31 00:03:25 +0800

    AC: 896

--- 896.monotonic-array.py
+++ 896.monotonic-array.py
@@ -4,4 +4,22 @@ class Solution(object):
         :type A: List[int]
         :rtype: bool
         """
-        
+        lena = len(A)
+        if lena <= 1: return True
+
+        idm = 0  # 0: unknown, 1: increasing, 2: decreasing
+
+        for i in range(1, lena):
+            if A[i] > A[i-1]:
+                if idm == 2:
+                    return False
+                idm = 1
+
+            if A[i] < A[i-1]:
+                if idm == 1:
+                    return False
+                idm = 2
+
+        return True
+
+
