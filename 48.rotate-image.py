25cea3eeea368de130249b750b316d9fde541f28
ishamo <qgmfky@gmail.com>
2019-05-31 00:25:46 +0800

    AC: 48

--- 48.rotate-image.py
+++ 48.rotate-image.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        
+        if not matrix or not matrix[0]: return
+
+        m, n = len(matrix), len(matrix[0])
+
+        for i in range(m):
+            for j in range(0, i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(m):
+            for j in range(n/2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
