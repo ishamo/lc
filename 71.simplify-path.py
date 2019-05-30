457c798ce24be042981ffc0dd52369bf51e8ba0b
ishamo <qgmfky@gmail.com>
2019-05-30 22:57:40 +0800

    AC: 71

--- 71.simplify-path.py
+++ 71.simplify-path.py
@@ -4,4 +4,25 @@ class Solution(object):
         :type path: str
         :rtype: str
         """
-        
+        newpath = path.replace('//', '/').replace('..', '*')
+        while newpath != path:
+            path = newpath
+            newpath = newpath.replace('//', '/').replace('..', '*')
+
+        path = path.strip('/')
+
+        path = path.split('/')
+
+        result = []
+        for p in path:
+            if p == '.':
+                continue
+            elif p == '*':
+                if result:
+                    result.pop(-1)
+            else:
+                result.append(p)
+
+        ret = '/' + '/'.join(result)
+        ret = ret.replace('*', '..')
+        return ret
