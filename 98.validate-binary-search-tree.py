27315cb775a58728ee1a6c5bdbeec64721d7b3fa
ishamo <qgmfky@gmail.com>
2019-05-31 15:27:28 +0800

    AC: 98

--- 98.validate-binary-search-tree.py
+++ 98.validate-binary-search-tree.py
@@ -6,9 +6,27 @@
 #         self.right = None
 
 class Solution(object):
+
     def isValidBST(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        
+        if not root: return True
+        ret = []
+
+        def inorder(root):
+            if not root: return
+            if root.left:
+                inorder(root.left)
+            ret.append(root.val)
+            if root.right:
+                inorder(root.right)
+
+        inorder(root)
+
+        lenr = len(ret)
+        for i in range(1, lenr):
+            if ret[i] <= ret[i-1]: return False
+
+        return True
