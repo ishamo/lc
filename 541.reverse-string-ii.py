224a8453e900d5f0d88889b2c39efc9779f33723
ishamo <qgmfky@gmail.com>
2019-05-31 14:53:54 +0800

    AC: 541

--- 541.reverse-string-ii.py
+++ 541.reverse-string-ii.py
@@ -5,4 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        
+        lens = len(s)
+        result = ''
+        for i in range(0, lens+1, 2*k):
+            result += s[i:i+k][::-1]
+            result += s[i+k:i+2*k]
+
+        return result
+#
+#
+# s = Solution()
+# astr = 'abcdefg'
+# print s.reverseStr(astr, 2)
+#
+#
