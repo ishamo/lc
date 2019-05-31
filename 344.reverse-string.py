ec95b93e8196d196248f0a63bbdce7c69d13fc0e
ishamo <qgmfky@gmail.com>
2019-05-31 15:06:56 +0800

    AC: 344

--- 344.reverse-string.py
+++ 344.reverse-string.py
@@ -4,4 +4,13 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        
+        if not s: return
+
+        lens = len(s)
+        i, j = 0, lens-1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+
+        return
