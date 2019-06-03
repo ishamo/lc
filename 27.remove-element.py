aceedaaf0a8aca0cea1d175c6717302f5829e76a
ishamo <qgmfky@gmail.com>
2019-06-03 14:46:08 +0800

    AC: 27

--- 27.remove-element.py
+++ 27.remove-element.py
@@ -5,4 +5,21 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        
+        if not nums: return 0
+
+        while True:
+            before = len(nums)
+            try:
+                nums.remove(val)
+            except:
+                pass
+            after = len(nums)
+            if after == before: break
+
+        return len(nums)
+#
+#
+# s = Solution()
+# alist = [3,2,2,3]
+# s.removeElement(alist, 3)
+# print(alist)
