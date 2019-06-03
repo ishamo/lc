7f789ad6fbfc70f40bece98d620b362def89880b
ishamo <qgmfky@gmail.com>
2019-06-03 15:08:04 +0800

    AC: 283

--- 283.move-zeroes.py
+++ 283.move-zeroes.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
+        lenn = len(nums)
+        count0 = 0
+        idx = 0
+        while idx < lenn:
+            if nums[idx] == 0:
+                count0 += 1
+            else:
+                if count0 > 0:
+                    nums[idx], nums[idx-count0] = nums[idx-count0], nums[idx]
+
+            idx += 1
