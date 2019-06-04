a72491b5085452f20dbc80776d83e7bcf2250321
ishamo <qgmfky@gmail.com>
2019-06-04 23:17:02 +0800

    AC: 101

--- 101.symmetric-tree.py
+++ 101.symmetric-tree.py
@@ -11,4 +11,23 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        
+        if not root or not root.left and not root.right: return True
+
+        new_tree = self.buildTree(root)
+
+        return self.sameTree(root, new_tree)
+
+    def buildTree(self, root):
+        if not root: return None
+        new_tree = TreeNode(root.val)
+        new_tree.left = self.buildTree(root.right)
+        new_tree.right = self.buildTree(root.left)
+        return new_tree
+
+    def sameTree(self, root, new_tree):
+        if not root and not new_tree: return True
+        if not root and new_tree or root and not new_tree: return False
+
+        if not root.val == new_tree.val: return False
+
+        return self.sameTree(root.left, new_tree.left) and self.sameTree(root.right, new_tree.right)
