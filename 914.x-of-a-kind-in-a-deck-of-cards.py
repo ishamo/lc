59abf4876c94ced9b470129add47257e54c2c74a
ishamo <qgmfky@gmail.com>
2019-06-05 13:05:53 +0800

    AC: 914

--- 914.x-of-a-kind-in-a-deck-of-cards.py
+++ 914.x-of-a-kind-in-a-deck-of-cards.py
@@ -4,4 +4,18 @@ class Solution(object):
         :type deck: List[int]
         :rtype: bool
         """
-        
+        table = {}
+        for d in deck:
+            if d not in table:
+                table[d] = 0
+            table[d] += 1
+
+        g = reduce(self.gcd, table.values())
+        if g < 2: return False
+
+        return True
+
+    def gcd(self, a, b):
+        if b != 0:
+            return self.gcd(b, a %  b)
+        return a
