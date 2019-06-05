a5a288b8bca624a5b1cc96a70a6d76bc30a047cb
ishamo <qgmfky@gmail.com>
2019-06-05 16:24:44 +0800

    AC: 922

--- 922.sort-array-by-parity-ii.py
+++ 922.sort-array-by-parity-ii.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type A: List[int]
         :rtype: List[int]
         """
-        
+        even, odd = [], []
+        for item in A:
+            if item % 2 == 0: even.append(item)
+            else: odd.append(item)
+
+        result = []
+        for i in range(len(even)):
+            result.append(even[i])
+            result.append(odd[i])
+
+        return result
+
