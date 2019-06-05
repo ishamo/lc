94172c13e955c691ade26e38532b309bcde7248b
ishamo <qgmfky@gmail.com>
2019-06-05 11:28:45 +0800

    AC: 704

--- 704.binary-search.py
+++ 704.binary-search.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
+        if not nums: return -1
+        lenn = len(nums)
+        l, r = 0, lenn - 1
+        while l <= r:
+            mid = (l + r) / 2
+            if nums[mid] == target: return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return -1
+
