4669df2a20a6fb946cde95aab7ae214bbdd4469a
ishamo <qgmfky@gmail.com>
2019-06-05 14:17:37 +0800

    AC: 929

--- 929.unique-email-addresses.py
+++ 929.unique-email-addresses.py
@@ -4,4 +4,13 @@ class Solution(object):
         :type emails: List[str]
         :rtype: int
         """
-        
+        tables = {}
+        for e in emails:
+            local, domain = e.split('@')
+            local = local.split('+')[0]
+            local = local.replace('.', '')
+            if domain not in tables:
+                tables[domain] = set()
+            tables[domain].add(local)
+
+        return sum([len(item) for item in tables.values()])
