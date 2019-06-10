381ff83c27e5d665ad70ec8d01ebb4f505aeff8c
ishamo <qgmfky@gmail.com>
2019-06-10 10:12:56 +0800

    AC: 561

--- 561.array-partition-i.py
+++ 561.array-partition-i.py
@@ -4,4 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums: return 0
+        nums.sort()
+        lenn = len(nums)
+        return sum([min(nums[i], nums[i+1]) for i in range(0, lenn, 2)])
