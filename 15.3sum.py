4b210fd95b34d8dd7112a0732cf4a5ebb4224326
ishamo <qgmfky@gmail.com>
2019-06-11 13:32:11 +0800

    AC: 15

--- 15.3sum.py
+++ 15.3sum.py
@@ -4,4 +4,36 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        result = []
+        lenn = len(nums)
+        if lenn < 3: return result
+
+        nums.sort()
+        # print('nums', nums)
+        for i in range(0, lenn-2):
+            if i > 0 and nums[i] == nums[i-1]: continue
+            j, k = i+1, lenn-1
+            while i < j < k:
+                sm = nums[i] + nums[j] + nums[k]
+                if sm == 0:
+                    result.append([nums[i], nums[j], nums[k]])
+
+                    k -= 1
+                    j += 1
+                    while k > j and nums[k+1] == nums[k]:
+                        k -= 1
+                    while j < k and nums[j-1] == nums[j]:
+                        j += 1
+
+                elif sm > 0:
+                    k -= 1
+                else:
+                    j += 1
+
+        return result
+#
+#
+# arr = [-1,0,1,2,-1,-4]
+#
+# s = Solution()
+# print s.threeSum(arr)
