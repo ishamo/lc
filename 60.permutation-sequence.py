8554115eebf26afc0bc038b122f4a5739fb7b70c
ishamo <qgmfky@gmail.com>
2019-06-12 13:15:47 +0800

    AC: 60

--- 60.permutation-sequence.py
+++ 60.permutation-sequence.py
@@ -5,4 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        
+        from itertools import permutations
+
+        pool = list(range(1, n+1))
+
+        j = 0
+        g = permutations(pool)
+        while j < k:
+            element = next(g)
+            j += 1
+
+        return ''.join([str(item) for item in element])
+#
+#
+# s = Solution()
+# print s.getPermutation(4, 9)
+#
