8c45af284c7ed2c257917338d4b16b8461fb67bc
ishamo <qgmfky@gmail.com>
2019-06-12 13:26:31 +0800

    AC: 129

--- 129.sum-root-to-leaf-numbers.py
+++ 129.sum-root-to-leaf-numbers.py
@@ -11,4 +11,22 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        
+        path = self.enumAllPath(root)
+
+        return sum([int(item) for item in path])
+
+    def enumAllPath(self, root):
+        path = []
+        self.helper(root, path)
+        return path
+
+    def helper(self, root, path, current=''):
+        if not root: return
+        current += str(root.val)
+        if not root.left and not root.right:
+            path.append(current)
+        if root.left:
+            self.helper(root.left, path, current)
+        if root.right:
+            self.helper(root.right, path, current)
+        return
