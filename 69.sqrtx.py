4222cc51f95333c6e23fff7ce6a2cc1066d92fca
ishamo <qgmfky@gmail.com>
2019-06-13 18:41:39 +0800

    AC: 69

--- 69.sqrtx.py
+++ 69.sqrtx.py
@@ -4,4 +4,23 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        
+        if x == 0: return 0
+        l, r = 1, x
+        while l <= r:
+            mid = (l+r)/2
+            multify = mid * mid
+            if multify == x: return mid
+            elif multify < x:
+                if (mid+1)*(mid+1)>x:
+                    return mid
+                l = mid
+            else:
+                r = mid
+
+        return 1
+#
+#
+# s = Solution()
+# print s.mySqrt(4)
+# print s.mySqrt(10)
+# print s.mySqrt(9)
