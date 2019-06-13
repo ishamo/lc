5597ca1259704e0f7687e9db1e2859cf6d182765
ishamo <qgmfky@gmail.com>
2019-06-13 19:44:54 +0800

    AC: 88

--- 88.merge-sorted-array.py
+++ 88.merge-sorted-array.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
@@ -7,4 +8,14 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        
+        nums1[m:m+n] = nums2
+        nums1[:m+n] = sorted(nums1[:m+n])
+
+
+
+
+# ✘ Wrong Answer
+# ✘ 20/59 cases passed (N/A)
+# ✘ testcase: '[4,5,6,0,0,0]\n3\n[1,2,3]\n3'
+# ✘ answer: [2,3,1,4,5,6]
+# ✘ expected_answer: [1,2,3,4,5,6]
