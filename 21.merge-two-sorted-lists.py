cb4a3fdeee7958b035dcf4428264836f8b4812cd
ishamo <qgmfky@gmail.com>
2019-06-13 14:55:00 +0800

    AC: 21

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -11,4 +11,25 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        
+        l1_values, l2_values = [], []
+        while l1:
+            l1_values.append(l1.val)
+            l1 = l1.next
+
+        while l2:
+            l2_values.append(l2.val)
+            l2 = l2.next
+
+        l1_values.extend(l2_values)
+        l1_values.sort()
+
+        len1 = len(l1_values)
+        record, head = None, None
+        for i in l1_values[::-1]:
+            head = ListNode(i)
+            head.next = record
+            record = head
+
+        return head
+
+
