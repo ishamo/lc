7fb85bf528a53f1aee413e5dc7b3ca0b1efc371f
ishamo <qgmfky@gmail.com>
2019-06-13 14:49:02 +0800

    AC: 7

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -4,4 +4,18 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        
+        x = str(x)
+        negtive = True if x[0] == '-' else False
+
+        if negtive:
+            x = -1 * int(x[1:][::-1])
+        else: x = int(x[::-1])
+
+        # print('x', x)
+        if x < -pow(2,31) or x > pow(2,31)-1: return 0
+        return x
+
+
+# x = -123
+# s = Solution()
+# print s.reverse(x)
