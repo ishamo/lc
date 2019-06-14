bb5ae9ef2772d4600d91be996951a19849a1d655
ishamo <qgmfky@gmail.com>
2019-06-14 15:14:15 +0800

    AC: 897

--- 897.increasing-order-search-tree.py
+++ 897.increasing-order-search-tree.py
@@ -11,4 +11,33 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
-        
+        if not root: return None
+
+        result = []
+
+        def inorder(root):
+            if not root: return
+            if root.left:
+                inorder(root.left)
+            result.append(root.val)
+            if root.right:
+                inorder(root.right)
+
+        inorder(root)
+
+        if not result: return None
+
+        head = TreeNode(result[0])
+        p = head
+        tail = None
+
+        for item in result[1:]:
+            tail = TreeNode(item)
+            p.right = tail
+            p = tail
+
+        if tail:
+            tail.right = None
+
+        return head
+
