7102b2a132db19376d0dd70627aef8db4e82799c
ishamo <qgmfky@gmail.com>
2019-06-14 15:02:06 +0800

    AC: 53

--- 53.maximum-subarray.py
+++ 53.maximum-subarray.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums: return 0
+        lenn = len(nums)
+        DP = [i for i in nums]
+
+        for i in range(1, lenn):
+            if DP[i-1] > 0:
+                DP[i] = DP[i-1] + nums[i]
+
+        return max(DP)
