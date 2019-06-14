327c909ff5fa8e821f88dac47573a20bb7f2164d
ishamo <qgmfky@gmail.com>
2019-06-14 14:06:34 +0800

    AC: 111

--- 111.minimum-depth-of-binary-tree.py
+++ 111.minimum-depth-of-binary-tree.py
@@ -11,4 +11,17 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        
+        if not root: return 0
+
+        # 不要算0就行了
+        lvalue = self.minDepth(root.left)
+        rvalue = self.minDepth(root.right)
+
+        if lvalue and rvalue:
+            return 1 + min(lvalue, rvalue)
+        elif lvalue:
+            return 1 + lvalue
+        elif rvalue:
+            return 1 + rvalue
+        else:
+            return 1
