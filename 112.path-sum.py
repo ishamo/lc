0125cc19d34ab050db2eef4952f7a2f0e9134da6
ishamo <qgmfky@gmail.com>
2019-06-17 16:00:36 +0800

    AC: 112

--- 112.path-sum.py
+++ 112.path-sum.py
@@ -12,4 +12,25 @@ class Solution(object):
         :type sum: int
         :rtype: bool
         """
-        
+        paths = []
+        self.travel_path(root, paths, [])
+        for p in paths:
+            if reduce(lambda a, b: a+b, p) == sum: return True
+
+        return False
+
+    def travel_path(self, root, paths, current):
+        if not root: return
+
+        current = current[:]
+        current.append(root.val)
+        if not root.left and not root.right:
+            paths.append(current)
+            return
+        else:
+            if root.left:
+                self.travel_path(root.left, paths, current)
+            if root.right:
+                self.travel_path(root.right, paths, current)
+
+
