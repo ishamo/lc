cdac9d9f18688351029c07e34492f7d7ebf2f291
ishamo <qgmfky@gmail.com>
2019-06-17 14:14:12 +0800

    AC: 257

--- 257.binary-tree-paths.py
+++ 257.binary-tree-paths.py
@@ -11,4 +11,24 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[str]
         """
-        
+        path = []
+        self.search_path(root, path, [])
+
+        result = []
+        for p in path:
+            result.append('->'.join(p))
+
+        return result
+
+    def search_path(self, root, path, current):
+        if not root: return
+        current = current[:]
+        current.append(str(root.val))
+        if not root.left and not root.right:
+            path.append(current)
+            return
+        else:
+            if root.left:
+                self.search_path(root.left, path, current)
+            if root.right:
+                self.search_path(root.right, path, current)
