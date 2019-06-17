e901ba5991b0df460a4efcd463621a83b130e5fa
ishamo <qgmfky@gmail.com>
2019-06-17 15:08:34 +0800

    AC: 155, 注意是>=, 有相同的最小值时，pop出去一个还能接着pop

--- 155.min-stack.py
+++ 155.min-stack.py
@@ -4,37 +4,48 @@ class MinStack(object):
         """
         initialize your data structure here.
         """
-        
+        self.stack = []
+        self.small_stack = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
-        
+        self.stack.append(x)
+        if not self.small_stack or self.small_stack[-1] >= x:
+            self.small_stack.append(x)
 
     def pop(self):
         """
         :rtype: None
         """
-        
+        if self.small_stack[-1] == self.stack[-1]:
+            self.small_stack.pop(-1)
+        self.stack.pop(-1)
 
     def top(self):
         """
         :rtype: int
         """
-        
+        return self.stack[-1]
+
 
     def getMin(self):
         """
         :rtype: int
         """
-        
+        return self.small_stack[-1]
+
 
 
 # Your MinStack object will be instantiated and called as such:
+# ["MinStack","push","push","push","getMin","pop","getMin"]
+# [[],[0],[1],[0],[],[],[]]
 # obj = MinStack()
-# obj.push(x)
+# obj.push(0)
+# obj.push(1)
+# obj.push(0)
+# param_1 = obj.getMin()
 # obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+# param_2 = obj.getMin()
