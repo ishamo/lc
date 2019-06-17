ebefa952cbb4d7a5d97e6134d17d6b96a5326889
ishamo <qgmfky@gmail.com>
2019-06-17 12:02:51 +0800

    AC: 66

--- 66.plus-one.py
+++ 66.plus-one.py
@@ -4,4 +4,4 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        
+        return [int(i) for i in list(str(int(''.join([str(item) for item in digits])) + 1))]
