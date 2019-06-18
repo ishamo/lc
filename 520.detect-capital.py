1ae9c8cdfd078f6cd2b0d060769bbdbe830bcc96
ishamo <qgmfky@gmail.com>
2019-06-18 18:10:11 +0800

    AC: 520

--- 520.detect-capital.py
+++ 520.detect-capital.py
@@ -4,4 +4,23 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        
+        lenw = len(word)
+
+        if lenw == 1: return True
+
+        if 'a' <= word[0] <= 'z':
+            for i in range(1, lenw):
+                if not 'a'<=word[i]<='z': return False
+
+        if 'A' <= word[0] <= 'Z':
+            if 'A' <= word[1] <= 'Z':
+                for i in range(2, lenw):
+                    if not 'A' <= word[i] <= 'Z': return False
+
+            if 'a' <= word[1] <= 'z':
+                for i in range(2, lenw):
+                    if not 'a' <= word[i] <= 'z': return False
+
+        return True
+
+
