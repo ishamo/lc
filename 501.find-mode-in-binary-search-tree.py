f4a94cbb130111703f3e4ce5417ed50b111f9a35
ishamo <qgmfky@gmail.com>
2019-06-18 13:24:36 +0800

    AC: 501

--- 501.find-mode-in-binary-search-tree.py
+++ 501.find-mode-in-binary-search-tree.py
@@ -11,4 +11,34 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
-        
+        result = []
+        if not root: return result
+
+        self.inorder(root, result)
+
+        table = {}
+        for r in result:
+            if r not in table:
+                table[r] = 0
+            table[r] += 1
+
+        mx = max(table.values())
+        ret = []
+        for k, v in table.items():
+            if v == mx: ret.append(k)
+
+        return ret
+
+    def inorder(self, root, result):
+        if not root:
+            return
+
+        if root.left:
+            self.inorder(root.left, result)
+
+        result.append(root.val)
+
+        if root.right:
+            self.inorder(root.right, result)
+
+
