d12c1f342051b728e01174c154f5d7d89a0d1616
ishamo <qgmfky@gmail.com>
2019-06-18 13:15:14 +0800

    AC: 784

--- 784.letter-case-permutation.py
+++ 784.letter-case-permutation.py
@@ -4,4 +4,23 @@ class Solution(object):
         :type S: str
         :rtype: List[str]
         """
-        
+        result = []
+        if not S: return result
+        lens = len(S)
+
+        i = 0
+
+        self.enum_every_char(S, i, lens, result, '')
+
+        return result
+
+    def enum_every_char(self, S, i, lens, result, current):
+        if i == lens:
+            result.append(current)
+            return
+        else:
+            if 'A' <= S[i].upper() <= 'Z':
+                self.enum_every_char(S, i+1, lens, result, current+S[i].lower())
+                self.enum_every_char(S, i+1, lens, result, current+S[i].upper())
+            else:
+                self.enum_every_char(S, i+1, lens, result, current+S[i])
