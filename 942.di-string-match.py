7d8f969d52ce7f7eb8f14913efc48f36860c918e
ishamo <qgmfky@gmail.com>
2019-06-19 10:16:11 +0800

    AC: 942

--- 942.di-string-match.py
+++ 942.di-string-match.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type S: str
         :rtype: List[int]
         """
-        
+        result = []
+        if not S: return result
+
+        mn, mx = 0, len(S)
+
+        for i in range(len(S)):
+            if S[i] == 'D':
+                result.append(mx)
+                mx -= 1
+            else:
+                result.append(mn)
+                mn += 1
+
+        result.append(mn)
+        return result
+
+
