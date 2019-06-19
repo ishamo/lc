ebf0f9abb8f83f3e074a244e27cb78db4c9376ec
ishamo <qgmfky@gmail.com>
2019-06-19 15:09:18 +0800

    AC: 206

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -3,6 +3,12 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+#
+#     def __repr__(self):
+#         return str(self.val)
+#
+#     def __str__(self):
+#         return self.__repr__()
 
 class Solution(object):
     def reverseList(self, head):
@@ -10,4 +16,31 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        
+        if not head or not head.next: return head
+
+        prev, cur = None, head
+        while cur:
+            # this is wrong, why?
+            # prev, cur, cur.next = cur, cur.next, prev
+            # that is right.
+            cur.next, cur, prev = prev, cur.next, cur
+
+        return prev
+
+
+#
+# n1 = ListNode(1)
+# n2 = ListNode(2)
+# n3 = ListNode(3)
+# n4 = ListNode(4)
+# n5 = ListNode(5)
+# n1.next = n2
+# n2.next = n3
+# n3.next = n4
+# n4.next = n5
+#
+# s = Solution()
+# p = s.reverseList(n1)
+# while p:
+#     print p.val
+#     p = p.next
