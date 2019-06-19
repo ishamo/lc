c16873fc5b9ad1411e493223454b882b7c884071
ishamo <qgmfky@gmail.com>
2019-06-19 13:05:21 +0800

    AC: 167

--- 167.two-sum-ii-input-array-is-sorted.py
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -5,4 +5,18 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        lenn = len(numbers)
+
+        i, j = 0, lenn-1
+        while i < j:
+            sm = numbers[i] + numbers[j]
+            if sm == target:
+                return [i+1, j+1]
+            elif sm > target:
+                j -= 1
+            else:
+                i += 1
+
+        return [-1, -1]
+
+
