1cdf02f548d4dc3a11e7c4024ef2a1dcd6612346
ishamo <qgmfky@gmail.com>
2019-06-19 10:31:41 +0800

    AC: 633

--- 633.sum-of-square-numbers.py
+++ 633.sum-of-square-numbers.py
@@ -4,4 +4,21 @@ class Solution(object):
         :type c: int
         :rtype: bool
         """
-        
+        if c < 0: return False
+
+        end = int(pow(c, 1.0/2))
+        left, right = 0, end
+
+        while left <= right:
+            t = left * left + right * right
+            if t == c: return True
+            elif t > c: right -= 1
+            else:
+                left += 1
+
+        return False
+
+
+# s = Solution()
+# s.judgeSquareSum(5)
+#
