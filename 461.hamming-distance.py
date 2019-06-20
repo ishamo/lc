8aa88c206dcf1ed4a9885ad0b68207acb7268f67
ishamo <qgmfky@gmail.com>
2019-06-20 18:22:41 +0800

    AC: 461

--- 461.hamming-distance.py
+++ 461.hamming-distance.py
@@ -5,4 +5,28 @@ class Solution(object):
         :type y: int
         :rtype: int
         """
-        
+        sx, sy = bin(x), bin(y)
+
+        sx, sy = sx[2:], sy[2:]
+        lenx, leny = len(sx), len(sy)
+
+        if lenx > leny:
+            sx, sy = sy, sx
+            lenx, leny = leny, lenx
+
+        # print(lenx, leny)
+        sx = '0' * (leny-lenx) + sx
+
+        cnt = 0
+        # print(sx, sy)
+        for i in range(leny):
+            if sx[i] != sy[i]: cnt += 1
+
+        return cnt
+
+#
+# s = Solution()
+# print s.hammingDistance(3, 1)
+#
+
+
