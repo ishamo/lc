65b53549f3a582e891593af0637372b06aa59525
ishamo <qgmfky@gmail.com>
2019-06-20 13:15:52 +0800

    AC: 844

--- 844.backspace-string-compare.py
+++ 844.backspace-string-compare.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type T: str
         :rtype: bool
         """
-        
+        return True if self.dismiss(T) == self.dismiss(S) else False
+
+    def dismiss(self, S):
+        while True:
+            olds = S
+            S = S.lstrip('#')
+            idx = S.find('#')
+            if idx > 0:
+                S = S[:idx-1] + S[idx+1:]
+
+            if olds == S: break
+
+        return S
