bcff063e820fdcd089199e01d5fe93849f2ae14c
ishamo <qgmfky@gmail.com>
2019-06-20 15:56:23 +0800

    AC: 821

--- 821.shortest-distance-to-a-character.py
+++ 821.shortest-distance-to-a-character.py
@@ -5,4 +5,18 @@ class Solution(object):
         :type C: str
         :rtype: List[int]
         """
-        
+        if not S: return []
+
+        index = []
+        for idx, item in enumerate(S):
+            if item == C: index.append(idx)
+
+        result = []
+
+        for idx, item in enumerate(S):
+            result.append(self.find_closest(idx, index))
+
+        return result
+
+    def find_closest(self, target, nums):
+        return min(map(lambda x: abs(x - target), nums))
