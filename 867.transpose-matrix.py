7205d862556f6cbfd6772e216834a64f4e6ee352
ishamo <qgmfky@gmail.com>
2019-06-20 14:12:47 +0800

    AC: 867

--- 867.transpose-matrix.py
+++ 867.transpose-matrix.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type A: List[List[int]]
         :rtype: List[List[int]]
         """
-        
+        if not A or not A[0]: return []
+
+        m, n = len(A), len(A[0])
+
+        ret = [[0 for i in range(m)] for j in range(n)]
+
+        for i in range(m):
+            for j in range(n):
+                ret[j][i] = A[i][j]
+
+        return ret
