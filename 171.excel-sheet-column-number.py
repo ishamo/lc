ac217e6adffaff2ad127ba652c02fb0db2b5231a
ishamo <qgmfky@gmail.com>
2019-06-24 11:55:15 +0800

    AC: 171

--- 171.excel-sheet-column-number.py
+++ 171.excel-sheet-column-number.py
@@ -4,4 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+        if not s: return 0
+
+        lens = len(s)
+        result = 0
+
+        for idx, char in enumerate(s):
+            result += (ord(char)-ord('A')+1) * pow(26, lens-idx-1)
+
+        return result
+#
+#
+# s = Solution()
+# print s.titleToNumber('A')
+# print s.titleToNumber('B')
+#
+
