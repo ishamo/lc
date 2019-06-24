cc5c0dc091ec9d86dd0bb7a818381e23360d4de2
ishamo <qgmfky@gmail.com>
2019-06-24 15:24:41 +0800

    AC: 500

--- 500.keyboard-row.py
+++ 500.keyboard-row.py
@@ -4,4 +4,19 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        
+        result = []
+        if not words: return result
+
+        for w in words:
+            if self.isvalid(w.lower()): result.append(w)
+
+        return result
+
+    def isvalid(self, w):
+        keymap = [
+            set(list('qwertyuiop')),
+            set(list('asdfghjkl')),
+            set(list('zxcvbnm'))
+        ]
+        return True if any(map(lambda x: set(list(w)).issubset(x), keymap)) else False
+
