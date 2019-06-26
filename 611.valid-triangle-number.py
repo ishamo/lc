4023bc7c611c408ce85bd0d94f16fc0b90d54d4a
ishamo <qgmfky@gmail.com>
2019-06-26 13:07:03 +0800

    AC: 611

--- 611.valid-triangle-number.py
+++ 611.valid-triangle-number.py
@@ -4,4 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        lenn = len(nums)
+        if lenn < 3: return 0
+
+        nums.sort()
+        count = 0
+        for third in range(2, lenn)[::-1]:
+            l, r = 0, third-1
+            while l < r:
+                if nums[l] + nums[r] <= nums[third]:
+                    l += 1
+                else:
+                    count += r - l
+                    r -= 1
+
+        return count
+
+#
+# nums = [2,2,3,4]
+# s = Solution()
+# print s.triangleNumber(nums)
