a05476806cf7a90733aa8b355bb57ded4a723009
ishamo <qgmfky@gmail.com>
2019-06-26 17:14:26 +0800

    AC: 504

--- 504.base-7.py
+++ 504.base-7.py
@@ -4,4 +4,31 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        
+        positive = True
+        if num < 0:
+            positive = False
+            num = -1 * num
+
+        if num == 0: return '0'
+
+        result = ''
+        while num >= 7:
+            a, b = num / 7, num % 7
+            result += str(b)
+            num = a
+
+        if num: result += str(num)
+
+        if not positive:
+            return '-' + result[::-1]
+        return result[::-1]
+#
+#
+# s = Solution()
+# print s.convertToBase7(100)
+#
+
+
+
+
+
