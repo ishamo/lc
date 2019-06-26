92d21f9ceaeee195ad9915679cf0b9341c3ace6e
ishamo <qgmfky@gmail.com>
2019-06-26 16:22:02 +0800

    77: 超时

--- 77.combinations.py
+++ 77.combinations.py
@@ -5,4 +5,25 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        
+        result = []
+        if k > n: return result
+
+        pool = list(range(1, n+1))
+
+        self.helper(pool, result, [], k)
+
+        return result
+
+    def helper(self, pool, result, current, k):
+        if len(current) == k:
+            result.append(current)
+            return
+
+        release = [i for i in pool if i not in current]
+        for it in release:
+            if not current or it > current[-1]:
+                self.helper(pool, result, current[:] + [it], k)
+
+
+s = Solution()
+print s.combine(20, 16)
