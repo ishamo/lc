3ae42f2d4ccae6ed8cb41e37e8c416ca473d9a17
ishamo <qgmfky@gmail.com>
2019-06-27 17:01:08 +0800

    AC: 93 虽然ac了这题，但是写法很丑陋，需要去discussion上面看看好的解法，但是现在太功利了，先把题量刷上去再看吧

--- 93.restore-ip-addresses.py
+++ 93.restore-ip-addresses.py
@@ -4,4 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
-        
+        result = []
+        lens = len(s)
+        if lens < 4: return result
+
+        self.helper(s, result, 0, [])
+        return list(set(result))
+
+    def helper(self, release, result, k, current):
+        if k == 4:
+            if not release and self.isvalid(current):
+                result.append('.'.join(current))
+                return
+        else:
+            for i in range(1, 4):
+                self.helper(release[i:], result, k+1, current[:] + [release[0:i]])
+
+    def isvalid(self, current):
+        if not len(current) == 4: return False
+        if any(map(lambda x: len(x) >= 2 and x.startswith('0'), current)): return False
+        if all(map(lambda x: x and 0 <= int(x) <= 255, current)): return True
+        return False
+#
+#
+# s = Solution()
+# print s.restoreIpAddresses("0000")
