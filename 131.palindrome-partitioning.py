5fe860acec71774d0a6889829044c39b146572fb
ishamo <qiandiao@yeah.net>
2019-06-28 12:07:03 +0800

    tmp: 131

--- 131.palindrome-partitioning.py
+++ 131.palindrome-partitioning.py
@@ -4,4 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
-        
+        result = []
+        if not s: return result
+        lens = len(s)
+
+        self.helper(s, 0, 1, lens-1, [], result)
+        return result
+
+    def helper(self, s, i, j, cut, current, result):
+        if not self.isvalid(s[i:j+1]): return
+        else:
+            current = current[:] + [s[i:j+1]]
+            if j == cut:
+                result.append(current)
+                return
+            else:
+                for k in range(2, len(s)-1-j):
+                    self.helper(s, j+1, j+k, len(s)-1, current, result)
+
+    def isvalid(self, s, i, j):
+        while s[i] == s[j] and i < j:
+            i += 1
+            j -= 1
+
+        return True if i >= j else False
+
