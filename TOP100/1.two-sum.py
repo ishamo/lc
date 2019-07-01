e7ce93969df7d2e2b4154f2e3382db61f6ba3be6
ishamo <qiandiao@yeah.net>
2019-07-01 16:03:34 +0800

    AC: 1

--- TOP100/1.two-sum.py
+++ TOP100/1.two-sum.py
@@ -5,4 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        if not nums: return []
+        table = {}
+        for idx, i in enumerate(nums):
+            if i not in table:
+                table[i] = []
+            table[i].append(idx)
+
+        for idx, n in enumerate(nums):
+            if target-n in table:
+                other = set(table[target-n]) - set([idx])
+                if other:
+                    return [idx, list(other)[0]]
+
+        return []
+
+
+
+
