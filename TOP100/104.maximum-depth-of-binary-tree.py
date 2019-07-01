f3b77c57f13760bc69bdaae498de48ade7f4a2b9
ishamo <qiandiao@yeah.net>
2019-07-01 16:19:57 +0800

    AC: 104

--- TOP100/104.maximum-depth-of-binary-tree.py
+++ TOP100/104.maximum-depth-of-binary-tree.py
@@ -11,4 +11,6 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        
+        if not root: return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
