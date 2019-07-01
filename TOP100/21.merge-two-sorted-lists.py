a18bc43c50a769029eb54f5e2b8f1e22f9aea58e
ishamo <qiandiao@yeah.net>
2019-07-01 15:43:52 +0800

    AC: 21

--- TOP100/21.merge-two-sorted-lists.py
+++ TOP100/21.merge-two-sorted-lists.py
@@ -11,4 +11,35 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        
+        if not l1: return l2
+        if not l2: return l1
+
+        head = None
+        if l1.val <= l2.val:
+            head = l1
+            l1 = l1.next
+        else:
+            head = l2
+            l2 = l2.next
+
+        p = head
+        while l1 and l2:
+            if l1.val <= l2.val:
+                p.next = l1
+                l1 = l1.next
+            else:
+                p.next = l2
+                l2 = l2.next
+
+            p = p.next
+
+        if l1:
+            p.next = l1
+        if l2:
+            p.next = l2
+
+        return head
+
+
+
+
