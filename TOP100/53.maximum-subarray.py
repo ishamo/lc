b1964ff047e1915834873aa449263f0b910435a4
ishamo <qiandiao@yeah.net>
2019-07-01 16:56:43 +0800

    AC: 53

--- TOP100/53.maximum-subarray.py
+++ TOP100/53.maximum-subarray.py
@@ -4,4 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums: return 0
+
+        DP = nums[:]
+        for idx, i in enumerate(nums[1:], 1):
+            if DP[idx-1] > 0:
+                DP[idx] = DP[idx-1] + i
+            else:
+                DP[idx] = i
+
+        return max(DP)
