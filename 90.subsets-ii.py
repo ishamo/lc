3793f3c0a134c5cd1a3734d47dd3585cb0deea8c
ishamo <qiandiao@yeah.net>
2019-07-01 14:56:46 +0800

    90, 同78一样，只是顺序稍有不同, 未AC

--- 90.subsets-ii.py
+++ 90.subsets-ii.py
@@ -4,4 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+
+        result = set()
+        if not nums: return list(result)
+
+        self.helper(nums, 0, [], result)
+        return [list(item) for item in result]
+
+    def helper(self, nums, i, current, result):
+        if i == len(nums):
+            result.add(tuple(current))
+            return
+        else:
+            self.helper(nums, i+1, current, result)
+            current.append(nums[i])
+            self.helper(nums, i+1, current, result)
+            current.pop(-1)
+#
+#
+# s = Solution()
+# print s.subsetsWithDup([1, 2, 2])
+#
