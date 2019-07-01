8a51bab361f41cabadea20ee4b66ae99e92996de
ishamo <qiandiao@yeah.net>
2019-07-01 16:17:33 +0800

    AC: 169

--- TOP100/169.majority-element.py
+++ TOP100/169.majority-element.py
@@ -4,4 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        count = 0
+        last = None
+        for i in nums:
+            if last is None:
+                last = i
+                count = 1
+            elif i == last:
+                count += 1
+            else:
+                count -= 1
+                if count == 0:
+                    last = None
+
+        return last
