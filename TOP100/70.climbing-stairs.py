ba2dfa52c2e395b39a7b9ec17f19aafa3e71b78b
ishamo <qiandiao@yeah.net>
2019-07-01 19:08:41 +0800

    AC: 70

--- TOP100/70.climbing-stairs.py
+++ TOP100/70.climbing-stairs.py
@@ -4,4 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        a, b = 1, 2
+        if n == 1: return 1
+        if n == 2: return 2
+        for i in range(3, n+1):
+            a, b = b, a+b
+
+        return b
+
