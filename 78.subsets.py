996888333f5b2fb2f189e29c9e2065c20153425f
ishamo <qiandiao@yeah.net>
2019-07-01 14:48:40 +0800

    简单的回溯算法，但是我的输入顺序跟期望的顺序不一致无法AC, 顺序问题不重要吧

--- 78.subsets.py
+++ 78.subsets.py
@@ -4,4 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+
+        result = []
+        if not nums: return result
+
+        self.helper(nums, 0, [], result)
+        return result
+
+    def helper(self, nums, i, current, result):
+        if i == len(nums):
+            result.append(current[:])
+            return
+        else:
+            self.helper(nums, i+1, current, result)
+            current.append(nums[i])
+            self.helper(nums, i+1, current, result)
+            current.pop(-1)
+#
+#
+# s = Solution()
+# print s.subsets([1, 2, 3])
