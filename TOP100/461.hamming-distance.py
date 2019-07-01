f6fa9282ed17416abbda255d4acaa77078145f93
ishamo <qiandiao@yeah.net>
2019-07-01 16:26:49 +0800

    AC: 461

--- TOP100/461.hamming-distance.py
+++ TOP100/461.hamming-distance.py
@@ -5,4 +5,12 @@ class Solution(object):
         :type y: int
         :rtype: int
         """
-        
+        strx = bin(x)[2:]
+        stry = bin(y)[2:]
+        strx = (32 - len(strx)) * '0' + strx
+        stry = (32 - len(stry)) * '0' + stry
+        count = 0
+        for i in range(32):
+            if not strx[i] == stry[i]:
+                count += 1
+        return count
