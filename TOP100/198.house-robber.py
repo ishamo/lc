1c70d451e92fc460800bfec049a683cbc890e85b
ishamo <sha_moon@126.com>
2019-07-02 14:08:08 +0800

    AC: 198

--- TOP100/198.house-robber.py
+++ TOP100/198.house-robber.py
@@ -4,4 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums: return 0
+
+        lenn = len(nums)
+
+        MP = [[0 for i in range(2)] for j in range(lenn)]
+
+        MP[0][0] = 0
+        MP[0][1] = nums[0]
+
+        for i in range(1, lenn):
+            MP[i][0] = max(MP[i-1][0], MP[i-1][1], 0)
+            MP[i][1] = max(MP[i-1][0], 0) + nums[i]
+
+        return max(0, MP[lenn-1][0], MP[lenn-1][1])
+# 
+# 
+# nums = [1,2,3,1]
+# s = Solution()
+# print s.rob(nums)
+# nums = [2,7,9,3,1]
+# print s.rob(nums)
+# 
