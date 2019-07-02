7ae2655506e76fa5ee854dc3f0af191aae24b73e
ishamo <sha_moon@126.com>
2019-07-02 03:25:29 +0100

    AC: 581

--- TOP100/581.shortest-unsorted-continuous-subarray.py
+++ TOP100/581.shortest-unsorted-continuous-subarray.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums: return 0
+        cp = nums[:]
+        cp.sort()
+        lens = len(cp)
+        i, j = 0, lens-1
+
+        while i <= j and cp[i] == nums[i]:
+            i += 1
+        while i <= j and cp[j] == nums[j]:
+            j -= 1
+
+        return j-i+1
