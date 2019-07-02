b0836e1788f2eab74900c69a835e55e051dc8b0e
ishamo <sha_moon@126.com>
2019-07-02 03:40:56 +0100

    AC: 121

--- TOP100/121.best-time-to-buy-and-sell-stock.py
+++ TOP100/121.best-time-to-buy-and-sell-stock.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        
+        if not prices: return 0
+
+        mx = 0
+        small = prices[0]
+
+        for i in prices[1:]:
+            if i < small: small = i
+            if i - small > mx: mx = i-small
+
+        return mx
+# 
+# 
+# s = Solution()
+# nums = [7,1,5,3,6,4]
+# print s.maxProfit(nums)
+# nums = [7,6,4,3,2,1]
+# print s.maxProfit(nums)
