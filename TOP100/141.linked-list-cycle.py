23a07e2248827629715ed56bd8f55f44b943419e
ishamo <sha_moon@126.com>
2019-07-02 02:51:51 +0100

    AC: 141

--- TOP100/141.linked-list-cycle.py
+++ TOP100/141.linked-list-cycle.py
@@ -10,4 +10,13 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        if not head: return False
+
+        first, second = head, head
+        while first.next and first.next.next:
+            first = first.next.next
+            second = second.next
+            if first == second: return True
+
+        return False
         
