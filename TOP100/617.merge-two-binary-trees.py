040a8a092528faa72217ab162d898e317c0bb07d
ishamo <sha_moon@126.com>
2019-07-03 08:48:57 +0800

    AC: 617

--- TOP100/617.merge-two-binary-trees.py
+++ TOP100/617.merge-two-binary-trees.py
@@ -12,4 +12,15 @@ class Solution(object):
         :type t2: TreeNode
         :rtype: TreeNode
         """
-        
+        if t1 and t2:
+            t1.val += t2.val
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            t1.right = self.mergeTrees(t1.right, t2.right)
+            return t1
+        elif t1:
+            return t1
+        elif t2:
+            return t2
+        else:
+            return None
+
