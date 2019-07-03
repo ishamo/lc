cbf180a2782e0bf1e4e7ca4a858a3ca5e6adbc76
ishamo <sha_moon@126.com>
2019-07-03 08:34:14 +0800

    AC: 438

--- TOP100/438.find-all-anagrams-in-a-string.py
+++ TOP100/438.find-all-anagrams-in-a-string.py
@@ -5,4 +5,35 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
+
+        result = []
+        if not s or not p: return result
+
+        template = {}
+        for i in p:
+            if i not in template:
+                template[i] = 0
+            template[i] += 1
+
+        origin = {}
+        for idx, c in enumerate(s[0:len(s)]):
+            if c not in origin:
+                origin[c] = 0
+            origin[c] += 1
+            if idx >= len(p):
+                origin[s[idx-len(p)]] -= 1
+
+            if self.isSame(template, origin): result.append(idx+1-len(p))
+        return result
+
+    def isSame(self, template, origin):
+        # print('template, origin', template, origin)
+        for key in template:
+            if key not in origin or origin[key] != template[key]: return False
+        return True
         
+# s = "abacbabc"
+# p = "abc"
+# S = Solution()
+# print S.findAnagrams(s, p)
+# print 's', 'p', s, p
