3bd74f013cd44f85b9e64c76ecca2f9ac032097f
ishamo <sha_moon@126.com>
2019-07-03 08:43:22 +0800

    AC: 448

--- TOP100/448.find-all-numbers-disappeared-in-an-array.py
+++ TOP100/448.find-all-numbers-disappeared-in-an-array.py
@@ -4,4 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        result = set(range(1, len(nums)+1))
+        return list(result.difference(set(nums)))
         
