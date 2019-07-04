8d9db871809f12b4818e5721c79dae3524aa92d0
ishamo <sha_moon@126.com>
2019-07-04 15:46:24 +0800

    AC: 46

--- TOP100/46.permutations.py
+++ TOP100/46.permutations.py
@@ -4,4 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+
+        result = []
+        self.helper(nums, [], result)
+        return result
+
+    def helper(self, nums, path, result):
+        if len(path) == len(nums):
+            result.append(path[:])
+            return result
+        else:
+            release = [i for i in nums if i not in path]
+            for r in release:
+                path.append(r)
+                self.helper(nums, path, result)
+                path.pop(-1)
