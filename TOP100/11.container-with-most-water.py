b6c8a42c93f3e390f5529024ed5f3166fef9a5dd
ishamo <sha_moon@126.com>
2019-07-05 10:57:24 +0800

    AC: 11

--- TOP100/11.container-with-most-water.py
+++ TOP100/11.container-with-most-water.py
@@ -4,4 +4,22 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        lenn = len(height)
+        if not height or lenn < 2: return 0
+
+        l, r = 0, lenn-1
+        mx_water = 0
+        while l < r:
+            area = (r - l) * min(height[r], height[l])
+            if area > mx_water:
+                mx_water = area
+
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return mx_water
+
+
         
