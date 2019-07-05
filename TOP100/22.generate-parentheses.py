99b094eaba527177057cc41a1747e764ef92f954
ishamo <sha_moon@126.com>
2019-07-05 10:03:26 +0800

    AC: 22

--- TOP100/22.generate-parentheses.py
+++ TOP100/22.generate-parentheses.py
@@ -4,4 +4,24 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        
+        result = []
+        if n == 0: return result
+
+        self.helper(n, n, '', result)
+
+        return result
+
+    def helper(self, lc, rc, path, result):
+        if lc > rc: return
+        elif lc < 0 or rc < 0: return
+        elif lc == 0 and rc == 0:
+            result.append(path)
+            return
+        else:
+            self.helper(lc-1, rc, path+'(', result)
+            self.helper(lc, rc-1, path+')', result)
+# 
+# 
+#         
+# s = Solution()
+# print s.generateParenthesis(3)
