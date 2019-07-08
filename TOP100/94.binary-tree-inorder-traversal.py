0ddc6383b724d5f99131e82403856c4fc18fa4c8
ishamo <qiandiao@yeah.net>
2019-07-08 15:55:43 +0800

    AC: 94

--- TOP100/94.binary-tree-inorder-traversal.py
+++ TOP100/94.binary-tree-inorder-traversal.py
@@ -11,4 +11,39 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
-        
+
+        # iterable
+        result = []
+        if not root: return result
+
+        stack = [root]
+        visited = set()
+        while stack:
+            current = stack[-1]
+            if current.left and current.left not in visited:
+                current = current.left
+                stack.append(current)
+                continue
+
+            result.append(current.val)
+            visited.add(current)
+            stack.pop(-1)
+
+            if current.right:
+                current = current.right
+                stack.append(current)
+                continue
+
+        return result
+#
+#
+# n1 = TreeNode(1)
+# n2 = TreeNode(2)
+# n3 = TreeNode(3)
+# n4 = TreeNode(4)
+# n5 = TreeNode(5)
+#
+# n1.left, n1.right = n2, n3
+# n2.left, n2.right = n4, n5
+# s = Solution()
+# print s.inorderTraversal(n1)
