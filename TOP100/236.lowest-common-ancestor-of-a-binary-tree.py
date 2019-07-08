e2bf229c4e5b24a8890cc0989a0685a804edb2bb
ishamo <qiandiao@yeah.net>
2019-07-08 16:25:39 +0800

    AC: 236

--- TOP100/236.lowest-common-ancestor-of-a-binary-tree.py
+++ TOP100/236.lowest-common-ancestor-of-a-binary-tree.py
@@ -13,4 +13,12 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        
+        if not root: return root
+        if root == p or root == q: return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right: return root
+        if not left: return right
+        if not right: return left
+        return root
+
