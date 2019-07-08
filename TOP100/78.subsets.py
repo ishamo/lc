ee466cfb318a2f128fb964f262fb056385674686
ishamo <qiandiao@yeah.net>
2019-07-08 16:39:40 +0800

    78, 不晓得它到底要个什么顺序。。。

--- TOP100/78.subsets.py
+++ TOP100/78.subsets.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        result = []
+        if not nums: return result
+        self.helper(nums, result, [], 0)
+        return result
+
+    def helper(self, nums, result, path, idx):
+        if idx == len(nums):
+            result.append(path[:])
+            return
+        else:
+            self.helper(nums, result, path[:], idx+1)
+            self.helper(nums, result, path[:] + [nums[idx]], idx+1)
