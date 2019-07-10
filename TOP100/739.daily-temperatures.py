61a9290c3f468c2dfaf823097632bf928002074d
ishamo <qiandiao@yeah.net>
2019-07-10 16:43:29 +0800

    AC: 739

--- TOP100/739.daily-temperatures.py
+++ TOP100/739.daily-temperatures.py
@@ -4,4 +4,21 @@ class Solution(object):
         :type T: List[int]
         :rtype: List[int]
         """
-        
+        result = [0 for i in range(len(T))]
+        stack = []
+        for idx, item in enumerate(T):
+            if not stack or stack[-1][1] >= item:
+                stack.append((idx, item))
+            else:
+                while stack and stack[-1][1] < item:
+                    index, val = stack.pop(-1)
+                    result[index] = idx - index
+
+                stack.append((idx, item))
+
+        return result
+#
+#
+# T = [73, 73, 75, 71, 69, 72, 76, 73]
+# s = Solution()
+# print s.dailyTemperatures(T)
