f8fe536e31bfb38479d1e5bea4f7964740c0c43f
ishamo <qiandiao@yeah.net>
2019-07-10 16:09:35 +0800

    279: 又超时，leetcode给了一个挺大的数，没办法，leetcode就是这么不讲道理，意思是说解这题必须用四数平方和定理。。。

--- TOP100/279.perfect-squares.py
+++ TOP100/279.perfect-squares.py
@@ -1,7 +1,22 @@
+import math
 class Solution(object):
+
     def numSquares(self, n):
         """
         :type n: int
         :rtype: int
         """
-        
+        if n == 0: return 0
+        DP = [5 for i in range(n+1)]
+        DP[0], DP[1] = 0, 1
+        for i in range(2, n+1):
+            for j in range(0, i):
+                q = int(math.sqrt(i-j))
+                if j + q*q == i:
+                    DP[i] = min(DP[i], DP[j]+1)
+
+        return DP[n]
+
+
+s = Solution()
+print s.numSquares(71)
