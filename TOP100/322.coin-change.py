51d67c29ed63815e1ade7d9ea863d476624394c2
ishamo <qiandiao@yeah.net>
2019-07-11 16:59:23 +0800

    AC: 322, 调整一下for语句顺序可以减少复杂度

--- TOP100/322.coin-change.py
+++ TOP100/322.coin-change.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Solution(object):
     def coinChange(self, coins, amount):
         """
@@ -5,4 +6,15 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        
+        if amount == 0: return 0
+        if not coins: return -1
+        coins.sort()
+
+        DP = [1000] * (amount+1)
+        DP[0] = 0
+        for i in range(amount+1):
+            for c in coins:
+                if not 0 <= i+c <= amount: break
+                DP[i+c] = min(DP[i]+1, DP[i+c])
+
+        return DP[amount] if DP[amount]<1000 else -1
