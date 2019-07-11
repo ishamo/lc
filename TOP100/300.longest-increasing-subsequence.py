982cadcc9fa7b216ad97079b1e90d1d910df82d7
ishamo <qiandiao@yeah.net>
2019-07-11 13:16:44 +0800

    AC: 300

--- TOP100/300.longest-increasing-subsequence.py
+++ TOP100/300.longest-increasing-subsequence.py
@@ -4,4 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums: return 0
+        DP = [1] * len(nums)
+
+        for idx, item in enumerate(nums):
+            for j in range(idx):
+                if nums[idx] > nums[j]:
+                    DP[idx] = max(DP[idx], DP[j] + 1)
+
+        return max(DP)
+#
+#
+# nums = [10,9,2,5,3,7,101,18]
+# s = Solution()
+# print s.lengthOfLIS(nums)
+#
