68fc4ee1106a66445066be77a92b327cb36b25ce
ishamo <qiandiao@yeah.net>
2019-07-11 10:36:38 +0800

    AC: 3

--- TOP100/3.longest-substring-without-repeating-characters.py
+++ TOP100/3.longest-substring-without-repeating-characters.py
@@ -4,4 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+        if not s: return 0
+        arr = [0] * 256
+        lens = len(s)
+        mx = 0
+        i, j = 0, 0
+        while i < lens and j < lens:
+            if arr[ord(s[j])] == 0:
+                arr[ord(s[j])] = 1
+                j += 1
+                if j-i > mx:
+                    mx = j-i
+
+            else:
+                arr[ord(s[i])] = 0
+                i += 1
+
+        return mx
+#
+# s = Solution()
+# print s.lengthOfLongestSubstring('pwwkew')
+# print s.lengthOfLongestSubstring('p')
