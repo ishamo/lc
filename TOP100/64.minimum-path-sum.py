2852528638e6b8e690159d72238d737ef2e30c6b
ishamo <qiandiao@yeah.net>
2019-07-12 18:14:00 +0800

    AC: 64

--- TOP100/64.minimum-path-sum.py
+++ TOP100/64.minimum-path-sum.py
@@ -4,4 +4,24 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        
+        if not grid or not grid[0]: return 0
+        m, n = len(grid), len(grid[0])
+
+        DP = [[0 for i in range(n+1)] for j in range(m+1)]
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if i == 1:
+                    DP[i][j] = DP[i][j-1] + grid[i-1][j-1]
+                elif j == 1:
+                    DP[i][j] = DP[i-1][j] + grid[i-1][j-1]
+                else:
+                    DP[i][j] = min(DP[i-1][j], DP[i][j-1]) + grid[i-1][j-1]
+
+        return DP[m][n]
+
+#
+# grid = [[1,3,1],[1,5,1],[4,2,1]]
+# s = Solution()
+# print s.minPathSum(grid)
+#
