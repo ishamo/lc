3486b6f34a508c1edc38346887bd609a76fa2a50
ishamo <qiandiao@yeah.net>
2019-07-12 11:09:00 +0800

    AC: 19, 这个代码写得很不错，几个判断都加的比较好，都是试出来的

--- TOP100/19.remove-nth-node-from-end-of-list.py
+++ TOP100/19.remove-nth-node-from-end-of-list.py
@@ -3,7 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+#
 class Solution(object):
     def removeNthFromEnd(self, head, n):
         """
@@ -11,4 +11,42 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        
+        if not head: return head
+
+        fast, slow = head, head
+        count = 1
+        while fast.next:
+            fast = fast.next
+            count += 1
+            if count > n: break
+
+        if count <= n: return head.next
+
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+
+        return head
+#
+#
+# n1 = ListNode(1)
+# n2 = ListNode(2)
+# n3 = ListNode(3)
+# n4 = ListNode(4)
+# n5 = ListNode(5)
+# n6 = ListNode(6)
+# n1.next = n2
+# n2.next = n3
+# n3.next = n4
+# n4.next = n5
+# n5.next = n6
+#
+# s = Solution()
+# head = s.removeNthFromEnd(n1, 2)
+# p = head
+# while p:
+#     print p.val
+#     p = p.next
+#
