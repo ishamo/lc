ece05abbeefc83a4349964a89095836f915ebc02
ishamo <qiandiao@yeah.net>
2019-07-12 16:21:28 +0800

    AC: 2

--- TOP100/2.add-two-numbers.py
+++ TOP100/2.add-two-numbers.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
@@ -11,4 +11,32 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        
+        if not l1: return l2
+        if not l2: return l1
+
+        head = ListNode(-1)
+        p = head
+
+        c = 0
+        while l1 and l2:
+            sm = c + l1.val + l2.val
+            value = sm % 10
+            c = sm / 10
+            p.next = ListNode(value)
+            l1 = l1.next
+            l2 = l2.next
+            p = p.next
+
+        l = l1 or l2
+        while l:
+            sm = c + l.val
+            value = sm % 10
+            c = sm / 10
+            p.next = ListNode(value)
+            p = p.next
+            l = l.next
+
+        if c:
+            p.next = ListNode(c)
+
+        return head.next
