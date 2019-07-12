feeba613a92f7a161f46832c6282fcf5cfc70340
ishamo <qiandiao@yeah.net>
2019-07-12 10:27:57 +0800

    AC: 49

--- TOP100/49.group-anagrams.py
+++ TOP100/49.group-anagrams.py
@@ -4,4 +4,29 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        
+        result = []
+        if not strs: return result
+        table = {}
+        for s in strs:
+            key = tuple(sorted(s))
+            if key not in table: table[key] = []
+            table[key].append(s)
+
+        for v in table.values():
+            result.append(list(v))
+
+        return result
+
+
+
+
+# 这。。。
+# Actual
+#   ✔ runtime: 24 ms
+#   ✘ answer: [["bat"],["eat","tea","ate"],["tan","nat"]]
+#   ✔ stdout: ''
+#
+# Expected
+#   ✔ runtime: 0 ms
+#   ✔ answer: [["bat"],["nat","tan"],["ate","eat","tea"]]
+#   ✔ stdout: ''
