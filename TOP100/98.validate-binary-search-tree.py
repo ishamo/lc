7a766d9b78e767d550e2fce63aca62815e0d5d4b
neil <qiandiao@yeah.net>
2019-07-14 11:08:51 +0800

    AC: 98

--- TOP100/98.validate-binary-search-tree.py
+++ TOP100/98.validate-binary-search-tree.py
@@ -11,4 +11,18 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
+
+        result = []
+        if not root: return True
+        self.inorder(root, result)
+        for idx, item in enumerate(result[1:], 1):
+            if not item > result[idx-1]: return False
+
+        return True
+
+    def inorder(self, root, result):
+        if not root: return
+        self.inorder(root.left, result)
+        result.append(root.val)
+        self.inorder(root.right, result)
         
