6bc4db2284403559e2608176052e04716fd60ebf
ishamo <qiandiao@yeah.net>
2019-07-22 11:34:25 +0800

    AC: 238

--- TOP100/238.product-of-array-except-self.py
+++ TOP100/238.product-of-array-except-self.py
@@ -4,4 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        
+        result = []
+        if not nums: return result
+
+        lenn = len(nums)
+        left, right = [], []
+        left.append(1)
+        right.append(1)
+        for idx, item in enumerate(nums):
+            left.append(left[-1] * nums[idx])
+            right.append(right[-1] * nums[lenn-idx-1])
+
+        right.reverse()
+
+        for i in range(lenn):
+            result.append(left[i] * right[i+1])
+
+        return result
+#
+#
+# print Solution().productExceptSelf([1,2,3,4])
+#
