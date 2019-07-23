11b7726208537f0e7d5d2dfeedbbd6255673757c
ishamo <sha_moon@126.com>
2019-07-23 09:02:01 +0800

    AC: 34

--- TOP100/34.find-first-and-last-position-of-element-in-sorted-array.py
+++ TOP100/34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -5,4 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        if not nums: return [-1, -1]
+
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = (l+r)/2
+            if nums[mid] == target:
+                left, right = mid, mid
+                while left-1>=0 and nums[left] == nums[left-1]:
+                    left -= 1
+                while right+1<len(nums) and nums[right] == nums[right+1]:
+                    right += 1
+                return [left, right]
+            elif nums[mid] > target:
+                r = mid-1
+            else:
+                l = mid+1
+        return [-1, -1]
+                
+
+# print Solution().searchRange([2,2], 3)
         
