605d19be15ece90aaf09b994098716f3dd84eb6a
ishamo <qiandiao@yeah.net>
2019-07-30 11:11:46 +0800

    AC: 560

--- 560.subarray-sum-equals-k.py
+++ 560.subarray-sum-equals-k.py
@@ -5,4 +5,21 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        
+        if not nums: return 0
+        sm = ans = 0
+        table = {}
+        table[0] = 1
+        for n in nums:
+            sm += n
+            if sm not in table:
+                table[sm] = 0
+            table[sm] += 1
+
+            if sm-k in table:
+                ans += table[sm-k]
+
+        return ans
+#
+#
+# print Solution().subarraySum([1,1,1],2)
+#
